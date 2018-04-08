6ac22b99518ba83ac5ddd4a566c4c08c03062740
Manicben <manicben20@gmail.com>
2018-04-08 16:36:56 +0000

    Last mod to octoclient_test

--- octoclient_test.py
+++ octoclient_test.py
@@ -1,6 +1,12 @@
 from octoclient import OctoClient
 from api_keys import URL,API_KEY
 
+# Specify URL and API key for Octoprint
+if URL is None:
+    URL = 'YOUR OCTOPRINT IP ADDRESS'
+if API_KEY is None:
+    API_KEY = 'YOUR OCTOPRINT API KEY'
+
 try:
     client = OctoClient(url=URL, apikey=API_KEY)
     print(client.version)
